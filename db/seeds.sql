5b3e86f2fc8dab97c4eb499465c173a18507642c
Brandon Kim <bkim377@gatech.edu>
2020-04-09 09:56:30 -0400

    Checkpoint 1 - laid out file structure for directories

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dave''s Double', 0);
