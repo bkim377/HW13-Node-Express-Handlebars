68e0d793f2ea80984e0982da0c2aeed20cc5ffcb
Brandon Kim <bkim377@gatech.edu>
2020-04-11 14:26:23 -0400

    Checkpoint 11 - added the delete function for all burgers

--- models/burger.js
+++ models/burger.js
@@ -16,6 +16,11 @@ updateOne: function(objColVals, condition, cb) {
       cb(res);
     });
   },
+deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
 module.exports = burger;
